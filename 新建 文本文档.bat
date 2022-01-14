f74d21e192e6bf0613336182957920b8449e6787
tianzhendong <1203886034@qq.com>
2022-01-15 00:02:39 +0800

    Auto commit.

--- 新建 文本文档.bat
+++ 新建 文本文档.bat
@@ -1,6 +1,5 @@
-@echo off #打开回显或关闭请求回显功能，off 可以改成 on。
+@echo on #打开回显或关闭请求回显功能，off 可以改成 on。
 @title bat execute git auto commit #运行时命令行窗口的 title
-C:
 cd C:\Users\12038\Nutstore\1\我的坚果云\NoteBooks #这里是要提交的项目目录，需要更改成自己的文件目录
 git add .
 git commit -m "Auto commit."
